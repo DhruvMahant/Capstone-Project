93ca5172fd28a7eb2d66d893f75ba15bed5f8fe3
DhruvMahant <dhruv_mahant@yahoo.com>
2020-11-17 10:28:06 -0500

    Initial commit

---
+++ admin-portal/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+
+import { AuthGuard } from './auth/auth.guard';
+import { ServicesComponent } from './services/services.component';
+import { CompaniesListComponent } from './companies-list/companies-list.component';
+import { ProductsComponent } from './products/products.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+ // { path: 'services', component: ServicesComponent, canActivate: [AuthGuard] },
+  { path: 'companies-list', component: CompaniesListComponent, canActivate: [AuthGuard] },
+  { path: 'products', component: ProductsComponent, canActivate: [AuthGuard] }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [AuthGuard]
+})
+export class AppRoutingModule { }
